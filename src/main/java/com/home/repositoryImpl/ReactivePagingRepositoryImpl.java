2894f13d65642a73c9d928fb7a7ea1ead76ff751
徐辉 <xuhui@wowdsgn.com>
2017-09-21 18:28:52 +0800

    add log

--- src/main/java/com/home/repositoryImpl/ReactivePagingRepositoryImpl.java
+++ src/main/java/com/home/repositoryImpl/ReactivePagingRepositoryImpl.java
@@ -1,6 +1,8 @@
 package com.home.repositoryImpl;
 
 import com.home.model.BaseHourse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,12 +10,15 @@ import reactor.core.publisher.Flux;
 
 public class ReactivePagingRepositoryImpl<T,ID> implements ReactivePagingRepository<T,ID> {
 
+    private static final Logger logger = LoggerFactory.getLogger(ReactivePagingRepositoryImpl.class);
+
     @Autowired
     PagingHourseRepository pagingHourseRepository;
 
     @Override
     public <T1 extends BaseHourse> Flux<T1> findByTypeAndIsDeleted(Pageable pageable, String status, String isDeleted) {
         Page<T1> page = pagingHourseRepository.findByTypeAndIsDeleted(pageable,status,isDeleted);
+        logger.info(""+page.getTotalElements());
         return Flux.fromStream(page.getContent().stream());
     }
 }
